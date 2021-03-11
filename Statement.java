6590649c4fd76dc0760f219f1c2d6d45c81ef0de
Fagner Alves <altino.fagner@gmail.com>
2021-03-11 00:35:26 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,20 @@
-public class Statement {
-    
+import java.util.Enumeration;
+public abstract class Statement {
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = getHeaderString(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += getBodyString(each);
+        }
+        result += getFooterString(aCustomer);
+        return result;
+    }
+
+    protected abstract String getHeaderString(Customer aCustomer);
+
+    protected abstract String getBodyString(Rental aRental);
+
+    protected abstract String getFooterString(Customer aCustomer);
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,28 +1,14 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
 
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = getHeaderString(aCustomer);
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += getBodyString(each);
-        }
-        result += getFooterString(aCustomer);
-        return result;
-    }
-
-
-    private String getHeaderString(Customer aCustomer) {
+    protected String getHeaderString(Customer aCustomer) {
         return "Rental Record for " + aCustomer.getName() + "\n";
     }
 
-    private String getBodyString(Rental each) {
+    protected String getBodyString(Rental each) {
         return "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(each.getCharge()) + "\n";
     }
 
-    private String getFooterString(Customer aCustomer) {
+    protected String getFooterString(Customer aCustomer) {
         String result;
         result = "Amount owed is " + String.valueOf(aCustomer.getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + 
